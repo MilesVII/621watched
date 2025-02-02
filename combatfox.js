ab006fea0efbf2199b703d0813d73553eec24db1
Miles Seventh <milesseventh@gmail.com>
2025-02-02 14:03:41 +0400

    Add additional selector just in case

--- combatfox.js
+++ combatfox.js
@@ -85,7 +85,7 @@ async function getDirty(page, storedTags, storedQueries, masterPreviews){
 	if (urls.length > 1){
 		let bar = createProgressbar(urls.length);
 		progressbarCallback = counter => bar.set(counter);
-		document.querySelector(".navigation").appendChild(bar.element);
+		(document.querySelector(".navigation") || document.querySelector("nav")).appendChild(bar.element);
 	}
 
 	let pages = await loadPages(urls, progressbarCallback);
